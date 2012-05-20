76d8c9b5a5514bde1241d6e037ba914ccbf239e3
Glenn Sandoval <glenn.sandoval@gmail.com>
2012-05-20 17:26:59 +1200

    Refactor Ray.cs

--- Ray.cs
+++ Ray.cs
@@ -1,13 +1,31 @@
 ﻿namespace RayTracer {
 
     public class Ray {
-        public Vector3 E;
-        public Vector3 D;
+        private Vector3 m_Direction;
+        private Vector3 m_Source;
 
         public Ray( Vector3 e, Vector3 d ) {
-            E = e;
-            D = d;
-            D.Normalize();
+            m_Source = e;
+            m_Direction = d;
+            m_Direction.Normalize();
+        }
+
+        public Vector3 Direction {
+            get {
+                return m_Direction;
+            }
+            set {
+                m_Direction = value;
+            }
+        }
+
+        public Vector3 Source {
+            get {
+                return m_Source;
+            }
+            set {
+                m_Source = value;
+            }
         }
     }
 }
