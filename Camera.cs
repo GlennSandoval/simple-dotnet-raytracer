483c51b4a17404f1d601b5279b31e91c2b577eb5
Glenn Sandoval <glenn.sandoval@gmail.com>
2012-10-13 14:09:16 +1300

    Added backing fields for camera properties. Camera still needs work.

--- Camera.cs
+++ Camera.cs
@@ -2,14 +2,36 @@
 
     public class Camera {
 
+        public Vector3 m_Location;
+        public Vector3 m_LookAt;
+
         public Vector3 Location {
-            get;
-            set;
+            get {
+                return m_Location;
+            }
+            set {
+                m_Location = value;
+            }
         }
 
         public Vector3 LookAt {
-            get;
-            set;
+            get {
+                return m_LookAt;
+            }
+            set {
+                m_LookAt = value;
+            }
+        }
+
+        private Vector3 zaxis;
+
+        private void Init(){
+            zaxis = m_LookAt - m_Location;
+        }
+
+        public Ray GetCameraRay( int x, int y ) {
+            Vector3 lookAt = new Vector3( x - Location.x, -( y - Location.y ), 1 - Location.z );
+            return new Ray( Location, lookAt );
         }
     }
 }
